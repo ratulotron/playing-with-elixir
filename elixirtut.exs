17c00e96cda4721d85a24b9542f83871867fd642
Ratul Minhaz <m@ratutul.xyz>
2017-10-21 00:26:38 +0600

    did several stuff

--- elixirtut.exs
+++ elixirtut.exs
@@ -17,6 +17,21 @@ defmodule M do
     IO.puts "First: #{String.first(my_stuff)}"
     IO.puts "Index 4: #{String.at(my_stuff, 4)}"
     IO.puts "Substring: #{String.slice(my_stuff, 3, 8)}"
+
+    IO.inspect String.split(longer_str, " ")
+    IO.puts String.reverse(longer_str)
+    IO.puts String.upcase(longer_str)
+    IO.puts String.downcase(longer_str)
+    IO.puts String.capitalize(longer_str)
+
+    4 * 10 |> IO.puts
+
+    IO.puts "5 + 4 = #{5+4}"
+    IO.puts "5 - 4 = #{5-4}"
+    IO.puts "5 * 4 = #{5*4}"
+    IO.puts "5 / 4 = #{5/4}"
+    IO.puts "5 div 4 = #{div(5,4)}"
+
   end
 
 end
