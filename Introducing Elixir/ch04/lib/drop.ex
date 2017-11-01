5cb6d929ee2c12afea909ddbb4398babc50cb729
ratulotron <m@ratul.xyz>
2017-11-01 18:07:38 +0600

    more experiments

--- Introducing Elixir/ch04/lib/drop.ex
+++ Introducing Elixir/ch04/lib/drop.ex
@@ -1,18 +1,27 @@
 defmodule Drop do
   def fall_velocity(planemo, distance) when distance >= 0 do 
     gravity = case planemo do
+      # The sign -> reads 'yields'
       :earth  -> 9.8
       :moon   -> 1.6
       :mars   -> 3.71
-    end
-    :math.sqrt(2 * gravity * distance)
-    
-    # case planemo do
-      # The sign -> reads 'yields'
-      # :earth  -> :math.sqrt(2 * 9.8 * distance)
-      # :moon   -> :math.sqrt(2 * 1.6 * distance)
-      # :mars   -> :math.sqrt(2 * 3.71 * distance)
       # _       -> :nada
-    # end
+    end
+    
+    velocity = :math.sqrt(2 * gravity * distance)
+    
+    if velocity > 20 do
+      IO.puts("Look out below!")
+    else
+      IO.puts("Reasonable...")
+    end
+
+    cond do
+      velocity == 0 -> :stable
+      velocity < 5 -> :slow
+      velocity >= 5 and velocity < 10 -> :moving
+      velocity >= 10 and velocity < 20 -> :fast
+      velocity >= 20 -> :speedy    
+    end
   end
 end
