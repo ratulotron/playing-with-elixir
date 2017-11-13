bf9d23c9500f6a3eed1dc3bf4175c2538f6c6dad
ratulotron <m@ratul.xyz>
2017-11-13 16:47:41 +0600

    more experiments

--- Introducing Elixir/ch04/lib/drop.ex
+++ Introducing Elixir/ch04/lib/drop.ex
@@ -25,3 +25,15 @@ defmodule Drop do
     end
   end
 end
+
+
+defmodule Recursion do
+  def countdown(from) when from > 0 do
+    IO.inspect(from)
+    countdown(from-1)
+  end
+
+  def countdown(from) do
+    IO.puts("blastoff!!")
+  end
+end
